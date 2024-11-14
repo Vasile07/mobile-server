e698c5e904b0ab3a28bb6b10012ea794de7281e1
Irimies Vasile <133998309+Vasile07@users.noreply.github.com>
2024-11-14 18:51:45 +0200

    final app

--- index.js
+++ index.js
@@ -17,6 +17,33 @@ const jwt = require('jsonwebtoken');
 const secretKey = '5a36e4f53d0d1f86725e7655b262054fb0fd320478c76883c60a35f2b679db5c61bacf0fe60ef539bd59a1cd7bcfacce051d9bf6926503fead7ede539ff1f17d439ee171026581cfa39633ef839914b0dbb837174b2b18a361cdf7fd5a289e088eaf5e28e5f68a314949fc43d1e4d45d75a8320a642079d711207fa1feb147b926856b2a99fe94f32aecc1666f74d5b6b4e6281d84743d3c81143e92c2014ee9f527adbde3c8c093389f2292fc834d4fd3cc12b163a616f5aad63965833e544917514e2127b25ad2ea7d29665f8c0af134f8b8652ec46a74f3380a65b99d311354192c7166783bf0677874c13e58932b7d9274416385fc6c2b504171b3422360';
 
 
+wss.on('connection', (ws) => {
+
+    ws.on('message', (message) => {
+        const data = JSON.parse(message);
+        // Handle authorization message
+        if (data.type === 'authorization') {
+            const {token} = data.payload;
+            jwt.verify(token, secretKey, (err, decoded) => {
+                if (err) {
+                    console.error('JWT verification failed:', err);
+                    ws.send(JSON.stringify({error: 'Unauthorized'}));
+                    ws.close();
+                    return;
+                }
+
+                ws.userId = parseInt(decoded.userId);
+                console.log('User authorized:', ws.userId);
+            });
+        }
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
+
+
 app.use(bodyparser());
 app.use(cors());
 app.use(async (ctx, next) => {
@@ -41,19 +68,19 @@ app.use(async (ctx, next) => {
 });
 
 class Animal {
-    constructor({id, name, species, birthdate, isWild, weight}) {
+    constructor({id, name, species, birthdate, isVaccinate, weight}) {
         this.id = id;
         this.name = name;
         this.species = species;
         this.birthdate = birthdate;
-        this.isWild = isWild;
+        this.isVaccinate = isVaccinate;
         this.weight = weight;
     }
 }
 
-const broadcast = data =>
+const broadcast = (data, clientId) =>
     wss.clients.forEach(client => {
-        if (client.readyState === WebSocket.OPEN) {
+        if (client.readyState === WebSocket.OPEN && client.userId === clientId) {
             client.send(JSON.stringify(data));
         }
     });
@@ -64,25 +91,48 @@ function formatDate(date) {
     return new Intl.DateTimeFormat('en-GB').format(new Date(date));
 }
 
-async function getAllAnimals() {
-    return knex('animals').select('*');
+async function getAllAnimals(userId) {
+    return knex('animals').select('*').where('ownerId', userId);
 }
 
 router.get('/animals', async (ctx) => {
-    const animals = await getAllAnimals();
+    const token = ctx.header.authorization.split(" ").pop();
+    const userId = jwt.decode(token).userId;
+    const animals = await getAllAnimals(userId);
     ctx.response.body = animals;
     ctx.response.status = 200;
 });
 
+async function getAllAnimalsFromPage(userId, pageNumber, entitiesOnPage) {
+    console.log("Page number: " + pageNumber);
+    const offset = (pageNumber - 1) * entitiesOnPage;
+    return knex('animals')
+        .select('*')
+        .where('ownerId', userId)
+        .orderBy('id')
+        .offset(offset)
+        .limit(entitiesOnPage);
+}
+
+router.get("/animals/page_number=:page_number", async (ctx) => {
+    const token = ctx.header.authorization.split(" ").pop();
+    const userId = jwt.decode(token).userId;
+    const entitiesOnPage = 8;
+    const pageNumber = parseInt(ctx.params.page_number, 10);
+    ctx.response.body = await getAllAnimalsFromPage(userId, pageNumber, entitiesOnPage);
+    ctx.response.status = 200;
+})
+
 async function getAnimalById(animalId) {
     return knex('animals')
         .select('*')
-        .where("id", animalId);
+        .where("id", animalId).first();
 }
+
 
 router.get('/animals/:id', async (ctx) => {
     const animalId = ctx.request.params.id;
-    const animal = getAnimalById(animalId);
+    const animal = await getAnimalById(animalId);
     console.log(animal)
     if (animal) {
         ctx.response.body = animal;
@@ -92,6 +142,119 @@ router.get('/animals/:id', async (ctx) => {
         ctx.response.status = 404; // NOT FOUND (if you know the resource was deleted, then return 410 GONE)
     }
 });
+
+async function addAnimalToDb(animal, userId) {
+    await knex('animals').insert({
+        name: animal.name,
+        species: animal.species,
+        birthdate: animal.birthdate,
+        isVaccinate: animal.isVaccinate,
+        weight: animal.weight,
+        ownerId: userId,
+    });
+}
+
+async function getLastAnimalInserted(userId) {
+    return knex('animals').select('*').where('ownerId', userId).orderBy('id', "desc").first();
+}
+
+router.post('/animals', async (ctx) => {
+    const animal = ctx.request.body;
+    const token = ctx.header.authorization.split(" ").pop();
+    const userId = jwt.decode(token).userId;
+    await addAnimalToDb(animal, userId);
+    const insertedAnimal = await getLastAnimalInserted(userId);
+
+    ctx.response.status = 200;
+
+    broadcast({event: 'created', payload: {animal: insertedAnimal}}, userId);
+});
+
+async function updateAnimal(animal, userId, animalId) {
+    try {
+        // Ensure the update is performed and return the number of rows affected
+        const result = await knex('animals') // Make sure to specify the table name
+            .where('id', animalId)
+            .update({
+                name: animal.name,
+                species: animal.species,
+                birthdate: animal.birthdate,
+                isVaccinate: animal.isVaccinate,
+                weight: animal.weight,
+                image: animal.image,
+                ownerId: userId,
+                latitude: animal.latitude,
+                longitude: animal.longitude,
+            });
+
+        // Check if any rows were updated
+        if (result === 0) {
+            throw new Error('Animal not found or not updated');
+        }
+
+        return result; // Optionally return the result
+    } catch (error) {
+        console.error('Error updating animal:', error);
+        throw error; // Propagate the error
+    }
+}
+
+
+router.put("/animals/update/:id", async (ctx) => {
+    const animal = ctx.request.body;
+    const token = ctx.header.authorization.split(" ").pop();
+    const userId = jwt.decode(token).userId;
+    const animalId = ctx.params.id;
+    await updateAnimal(animal, userId, animalId);
+    ctx.status = 200;
+})
+
+
+async function getAllSpecies() {
+    return knex('animals').distinct('species');
+}
+
+router.get('/species', async (ctx) => {
+    try {
+        const species = await getAllSpecies();
+        ctx.response.body = species;
+        ctx.response.status = 200;
+        ctx.response.type = 'application/json'; // Optional: sets the Content-Type
+    } catch (error) {
+        ctx.response.status = 500;
+        ctx.response.body = {message: 'An error occurred while fetching species.'};
+    }
+});
+
+
+async function getAllAnimalsBySpecies(species, userId) {
+    if (species === "undefined")
+        return knex('animals').select('*').where('ownerId', userId);
+    else
+        return knex('animals').select('*').where('ownerId', userId).andWhere('species', species);
+}
+
+router.get('/animals/filter/species=:species', async (ctx) => {
+    const token = ctx.header.authorization.split(" ").pop();
+    const userId = jwt.decode(token).userId;
+    const species = ctx.request.params.species;
+    console.log("Start filter species " + species);
+    ctx.response.body = await getAllAnimalsBySpecies(species, userId);
+    ctx.response.status = 200;
+});
+
+async function getAllAnimalsSearch(userId) {
+    return knex('animals')
+        .select('*')
+        .where('ownerId', userId);
+}
+
+router.get('/search', async (ctx) => {
+    const token = ctx.header.authorization.split(" ").pop();
+    const userId = jwt.decode(token).userId;
+    ctx.response.body = await getAllAnimalsSearch(userId);
+    ctx.response.status = 200;
+})
 
 
 async function findUserByEmailAndPassword(email, password) {
@@ -104,18 +267,17 @@ async function findUserByEmailAndPassword(email, password) {
 
 router.post("/users/login", async (ctx) => {
     const {email, password} = ctx.request.body;
-    console.log(email, password);
 
     if (!email || !password) {
         ctx.response.body = {message: "Email and password are mandatory"}
-        ctx.response.body = 400; // BAD REQUEST
+        ctx.response.status = 400; // BAD REQUEST
         return;
     }
 
     const user = await findUserByEmailAndPassword(email, password);
 
     if (user) {
-        const token = jwt.sign({userId: user.id}, secretKey, {expiresIn: "1h"});
+        const token = jwt.sign({userId: user.id}, secretKey);
         ctx.response.body = {token: token};
         ctx.response.status = 200;
     } else {
@@ -124,97 +286,61 @@ router.post("/users/login", async (ctx) => {
     }
 });
 
+function verifyToken(token) {
+    try {
+        return jwt.verify(token, secretKey);
+    } catch (error) {
+        // Handle specific JWT errors if needed
+        throw new Error("Invalid JWT");
+    }
+}
+
+function decryptToken(token) {
+    return jwt.decode(token);
+}
+
+function findUserById(userId) {
+    return knex('users').where("id", userId).first();
+}
+
+router.post("/users/verify", async (ctx) => {
+    const {token} = ctx.request.body;
+
+    if (!token) {
+        ctx.response.body = {message: "No token received"};
+        ctx.response.status = 400; // BAD REQUEST
+        return;
+    }
+
+    try {
+        verifyToken(token); // This will throw an error if the token is invalid
+    } catch (error) {
+        ctx.response.body = {message: "Invalid JWT"};
+        ctx.response.status = 400; // BAD REQUEST
+        return;
+    }
+
+    const {userId} = decryptToken(token);
+    ctx.response.body = await findUserById(userId);
+    ctx.response.status = 200;
+});
+
 /** FOR WEBSOCKET **/
-setInterval(() => {
-    const animal = new Animal({
-        id: 5,
-        name: "NEW",
-        species: "SPECIES",
-        birthdate: formatDate(Date.now()),
-        isWild: true,
-        weight: 20
-    })
-    console.log("New Animal: ", animal)
-    broadcast({event: 'created', payload: {animal}});
-}, 5000);
+// setInterval(() => {
+//     const animal = new Animal({
+//         id: 5,
+//         name: "NEW",
+//         species: "SPECIES",
+//         birthdate: formatDate(Date.now()),
+//         isVaccinate: true,
+//         weight: 20
+//     })
+//     console.log("New Animal: ", animal)
+//     broadcast({event: 'created', payload: {animal}},1);
+// }, 5000);
 
 app.use(router.routes());
+
 app.use(router.allowedMethods());
 
 server.listen(3000);
-
-
-// const validateAnimal = (animal) => {
-//     if(!animal.name || !animal.species || !animal.birthdate || !animal.isWild || !animal.weight || animal.weight > 0)
-//         return false;
-//     return true;
-// }
-
-// const createItem = async (ctx) => {
-//     const animal = ctx.request.body;
-//     if (validateAnimal(animal)) { // validation
-//         ctx.response.body = { message: 'Data provided is not valid' };
-//         ctx.response.status = 400; //  BAD REQUEST
-//         return;
-//     }
-//     animal.id = `${parseInt(lastId) + 1}`;
-//     lastId = item.id;
-//     item.date = new Date();
-//     item.version = 1;
-//     items.push(item);
-//     ctx.response.body = item;
-//     ctx.response.status = 201; // CREATED
-//     broadcast({ event: 'created', payload: { item } });
-// };
-//
-// router.post('/item', async (ctx) => {
-//     await createItem(ctx);
-// });
-
-// router.put('/item/:id', async (ctx) => {
-//     const id = ctx.params.id;
-//     const item = ctx.request.body;
-//     item.date = new Date();
-//     const itemId = item.id;
-//     if (itemId && id !== item.id) {
-//         ctx.response.body = { message: `Param id and body id should be the same` };
-//         ctx.response.status = 400; // BAD REQUEST
-//         return;
-//     }
-//     if (!itemId) {
-//         await createItem(ctx);
-//         return;
-//     }
-//     const index = items.findIndex(item => item.id === id);
-//     if (index === -1) {
-//         ctx.response.body = { message: `item with id ${id} not found` };
-//         ctx.response.status = 400; // BAD REQUEST
-//         return;
-//     }
-//     const itemVersion = parseInt(ctx.request.get('ETag')) || item.version;
-//     if (itemVersion < items[index].version) {
-//         ctx.response.body = { message: `Version conflict` };
-//         ctx.response.status = 409; // CONFLICT
-//         return;
-//     }
-//     item.version++;
-//     items[index] = item;
-//     lastUpdated = new Date();
-//     ctx.response.body = item;
-//     ctx.response.status = 200; // OK
-//     broadcast({ event: 'updated', payload: { item } });
-// });
-//
-// router.del('/item/:id', ctx => {
-//     const id = ctx.params.id;
-//     const index = items.findIndex(item => id === item.id);
-//     if (index !== -1) {
-//         const item = items[index];
-//         items.splice(index, 1);
-//         lastUpdated = new Date();
-//         broadcast({ event: 'deleted', payload: { item } });
-//     }
-//     ctx.response.status = 204; // no content
-// });
-
-
